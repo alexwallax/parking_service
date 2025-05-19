bf420816a09cb1a8ff9b16ba7b27e1b78e4e6ad5
Alex <alexwallax@gmail.com>
2025-05-19 00:00:18 -0300

    Autentication 04

--- vehicles/views.py
+++ vehicles/views.py
@@ -1,3 +1,4 @@
+from rest_framework.permissions import DjangoModelPermissions
 from rest_framework import viewsets
 
 from vehicles.models import VehicleType, Vehicle 
@@ -7,8 +8,10 @@ from vehicles.serializers import VehicleTypeSerializer, VehicleSerializer
 class VehicleTypeViewSet(viewsets.ModelViewSet):
     queryset = VehicleType.objects.all()
     serializer_class = VehicleTypeSerializer
+    permission_classes = [DjangoModelPermissions]
 
 
 class VehicleViewSet(viewsets.ModelViewSet):
     queryset = Vehicle.objects.all()
     serializer_class = VehicleSerializer
+    permission_classes = [DjangoModelPermissions]
