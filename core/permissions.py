beb6b13c32ed44ffe3a2c1f7ef8bce5e1ebe61f1
Alex <alexwallax@gmail.com>
2025-05-20 21:20:24 -0300

    Autentication 06

--- core/permissions.py
+++ core/permissions.py
@@ -1,22 +1,21 @@
 from rest_framework import permissions
 
-#class IsOwnerOfVehicleOrRecord(permissions.BasePermission):
-
-#    def has_object_permission(self, request, view, obj):
-#        user = request.user
-
-#        if hasattr(obj, 'owner'):
-#            return obj.owner and obj.owner.user == user
-        
-#        if hasattr(obj, 'vehicle') and hasattr(obj.vehicle, 'owner'):
-#            return obj.vehicle.owner and obj.vehicle.owner.user == user
-        
-#        return False
-    
-
 class IsOwnerOfVehicleOrRecord(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
         user = request.user
-        has_permissions = obj.owner == user
-        return has_permissions
+
+        if hasattr(obj, 'owner'):
+            return obj.owner and obj.owner.user == user
+        
+        if hasattr(obj, 'vehicle') and hasattr(obj.vehicle, 'owner'):
+            return obj.vehicle.owner and obj.vehicle.owner.user == user
+        
+        return False
+    
+
+#class IsOwnerOfVehicleOrRecord(permissions.BasePermission):
+
+#    def has_object_permission(self, request, view, obj):
+#        has_permissions = obj.owner.user == request.user
+#        return has_permissions
